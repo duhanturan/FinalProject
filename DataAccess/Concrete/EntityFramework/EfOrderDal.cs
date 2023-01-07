5d9eabc43219b130a16749aff9640953c9f9bce4
DUHAN-TURAN <dduhanturann@gmail.com>
2023-01-07 14:10:27 +0300

    FinalProject 07.01.2023 Çalışma

---
+++ DataAccess/Concrete/EntityFramework/EfOrderDal.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfOrderDal:EfEntityRepositoryBase<Order,NorthwindContext>,IOrderDal
+    {
+
+    }
+}
